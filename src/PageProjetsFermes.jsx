684ce4e8334cdb02699a4708d3ee8e564755bd54
jobrie31 <jobrie31@hotmail.com>
2026-01-24 00:53:50 -0500

    26

--- src/PageProjetsFermes.jsx
+++ src/PageProjetsFermes.jsx
@@ -1199,7 +1199,7 @@ export function CloseProjectWizard({ projet, open, onCancel, onClosed, startAtSu
 
       const sendInvoiceEmail = httpsCallable(functions, "sendInvoiceEmail");
 
-      const toEmail = ["jlabrie@styro.ca"];
+      const toEmail = ['pieces@gyrotech.ca', 'ventes@gyrotech.ca', 'service@gyrotech.ca', 'tlemieux@gyrotech.ca'];
 
       await sendInvoiceEmail({
         projetId: projet.id,
